0e67276cca813b3c4d22246d8455aa66187195c2
cotoflux <guaupelu@hotmail.com>
2019-12-05 00:29:56 +0100

    Initial Boiler Plate PHP

--- src/Calcula.php
+++ src/Calcula.php
@@ -4,19 +4,7 @@ namespace App;
 
 class Calcula
 {
-    public $valorInicial = 0;
-    public $valorTotal;
 
-    public function Suma($valor)
-    {
-        $this->valorTotal = $this->valorInicial + $valor;
-        return $this->valorTotal;
-    }
-
-    public function Reset()
-    {
-        return $this->valorInicial;
-    }
 }
 
 
--- test/CalculaTest.php
+++ test/CalculaTest.php
@@ -9,27 +9,7 @@ class CalculaTest extends TestCase
 
         public function testValorInicial()
         {
-            $primerValor = new Calcula();
-            $valorEsperado = 0;
-            $valorObtenido = $primerValor->valorInicial;
-            $this->assertEquals($valorEsperado, $valorObtenido);
-        }
-
-        public function testSumaCorrectamente()
-        {
-            $segundoValor = new Calcula();
-            $valorEsperado = 5;
-            $valorObtenido = $segundoValor->Suma(5);
-            $this->assertEquals($valorEsperado, $valorObtenido);
-
-        }
-
-        public function testResetea()
-        {
-            $reset = new Calcula();
-            $valorEsperado = 0;
-            $valorObtenido = $reset->Reset();
-            $this->assertEquals($valorEsperado, $valorObtenido);
+            $this->assertEquals($expected $real);
         }
 
 
